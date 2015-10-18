428cf9066aac089bc2326db5bfdcbfb93d10c3e6
Aaron Parecki <aaron@parecki.com>
2015-10-18 15:46:58 -0700

    map api uses get or post

--- controllers/main.php
+++ controllers/main.php
@@ -9,9 +9,9 @@ $app->get('/map', function() use($app) {
   return Savant\render('map');
 });
 
-$app->get('/map/img', function() use($app) {
+$app->map('/map/img', function() use($app) {
   $params = $app->request()->params();
   $app->response['Content-type'] = 'image/png';
   $assetPath = dirname(__FILE__) . '/../p3k/map/images';
   $map = p3k\geo\StaticMap\generate($params, null, $assetPath);
-});
+})->via('GET','POST');
